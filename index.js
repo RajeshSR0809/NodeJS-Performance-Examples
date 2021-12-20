cb8373e9aaa6cdcb7540b4f0af4ca1ecb2b13350
RajeshSR0809 <srrajesh.svpur@gmail.com>
2021-12-20 21:44:55 +0530

    block main thread

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve('pages/index.html'));
 });
 
+app.get('/blockMainThread', (req, res) => {
+  doWork(5000);
+  res.sendFile(path.resolve('pages/index.html'));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+/* 
+  blocking the main thread with cpu intensive taks
+  open the more than 3 browsers to see the effect of 
+  blocking the main thread
+
+  duration: in ms
+*/
+function doWork(duration) {
+  let start = Date.now();
+  while (Date.now() - start < duration) {}
+}
